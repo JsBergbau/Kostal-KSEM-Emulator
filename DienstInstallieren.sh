3f667c104a968eab6522bcfb138da9fb8c4c4fe8
JsBergbau <37013344+JsBergbau@users.noreply.github.com>
2026-04-27 15:13:43 +0200

    fixed ksem-Service Installation

--- DienstInstallieren.sh
+++ DienstInstallieren.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
-sudo install -m 644 /etc/systemd/system/
+sudo install -m 644 "$DIR/ksem.service" /etc/systemd/system/ksem.service
 systemctl daemon-reload
 systemctl enable ksem.service
 systemctl start ksem.service
